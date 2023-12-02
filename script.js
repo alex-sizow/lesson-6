33ebea13f96b49ab74a836554db364562818099a
Alex Sizov <alex_sizow@outlook.com>
2023-12-02 16:24:02 +0300

    new lesson with delete logic

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-console.log('HELLO');
+let count = 1;
 
-function addText(text) {
-	let input = document.getElementById('myInput').value;
-	let text = document.createTextNode(input);
-	let li = document.createElement('li');
+const deleteText = c => {
+	let element = document.getElementById(c - 1);
+	console.log(element, count);
+	element.remove();
+};
+
+function addText() {
+	const input = document.getElementById('myInput').value;
+	// здесь сизвлекли текст из input
+	const text = document.createTextNode(input);
+	// здесь подготовили текст для li
+	const li = document.createElement('li');
+	// создали элемент li
 	li.append(text);
+	// добавили текст (text) в li
 
+	const button = document.createElement('button');
+	
+	const textButton = document.createTextNode('❌');
+	button.append(textButton);
+
+	button.addEventListener('click', function () {
+		deleteText(count);
+	});
+
+	li.append(button);
+
+	button.id = count;
+	li.id = count;
+
+	count++;
 	document.getElementById('list').append(li);
-
 	document.getElementById('myInput').value = '';
 }
