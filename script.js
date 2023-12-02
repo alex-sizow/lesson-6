bcde106182546e6ce55738c57badf7e93c4fc881
Alex Sizov <alex_sizow@outlook.com>
2023-12-02 17:46:59 +0300

    create delete element

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-let count = 1;
-
-const deleteText = c => {
-	let element = document.getElementById(c - 1);
-	console.log(element, count);
+const deleteText = id => {
+	let element = document.getElementById(id);
+	console.log(num);
 	element.remove();
 };
+// функция удаления
 
 function addText() {
 	const input = document.getElementById('myInput').value;
-	// здесь сизвлекли текст из input
-	const text = document.createTextNode(input);
-	// здесь подготовили текст для li
+	// здесь извлекаем текст из input по id
+
+	if (input === '') {
+		return;
+	}
+	// не выводится элемент если нет текста в input
+
+	const textLi = document.createTextNode(input);
+	// полготавливаем текст для записи в li
 	const li = document.createElement('li');
-	// создали элемент li
-	li.append(text);
-	// добавили текст (text) в li
+	// создаем элемент li
+	li.append(textLi);
+	// добавляем текст в элемент li выше
+	const idDate = new Date().getTime();
+	// уникальный id по дате
+
+	li.id = idDate;
+	// добавляем уникальный id с помощью даты
 
 	const button = document.createElement('button');
-	
+	// создаю элемент button
 	const textButton = document.createTextNode('❌');
+	// создаю крестик для кнопки
 	button.append(textButton);
+	// добавляю смайлик крестика в кнопку
 
 	button.addEventListener('click', function () {
-		deleteText(count);
+		deleteText(idDate);
 	});
+	// добавляем функцию удаления
 
 	li.append(button);
 
-	button.id = count;
-	li.id = count;
-
-	count++;
 	document.getElementById('list').append(li);
+	// добавляем элемент li в список по id list
+
 	document.getElementById('myInput').value = '';
 }
